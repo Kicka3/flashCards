a59185c7ddaa9754c767bff3e49b16359338bdd3
Kirill <kirill_ozornin@mail.ru>
2024-03-28 23:59:20 +0500

    feat: add btn styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { Button } from '@/components/ui/button'
 export function App() {
   return (
     <div>
-      {/*<Button>Btn</Button>*/}
       <Button icon={<LogOut />}>Btn with icon</Button>
-      {/*<Button as={'a'} href={'#'}>*/}
-      {/*  Link Btn*/}
-      {/*</Button>*/}
     </div>
   )
 }
