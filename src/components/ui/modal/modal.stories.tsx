41479cf8faf262f2faf4835f1b7828335b9eb230
Kirill <kirill_ozornin@mail.ru>
2024-04-14 22:43:54 +0500

    FLAS-12: update modal.tsx and styles 2

--- src/components/ui/modal/modal.stories.tsx
+++ src/components/ui/modal/modal.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { Typography } from '@/components/ui'
 import { Modal } from '@/components/ui/modal/modal'
 
 const meta = {
@@ -12,10 +13,57 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const ModalWitchText: Story = {
+export const ModalWitchTitle: Story = {
   args: {
     onOpenChange: (open: boolean) => console.log('is open'),
-    open: false,
+    open: true,
+    title: 'Title modal',
+  },
+}
+
+export const ModalWitchContent: Story = {
+  args: {
+    children: (
+      <Typography variant={'body1'}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab blanditiis ducimus eos expedita
+        incidunt molestias nam neque placeat qui reprehenderit, tempore veniam! A, ad aspernatur
+        dignissimos ex, fuga fugiat ipsam labore laborum maiores non praesentium quae quam
+        repellendus. Adipisci commodi consequatur est fugiat fugit ipsam quidem sed sint! Alias
+        animi, aperiam commodi eum, eveniet exercitationem, fuga illo itaque magni molestias odit
+        provident voluptate voluptatem. Alias, aliquid aperiam dolores et eveniet exercitationem
+        explicabo nemo odit, recusandae, rerum sed similique sit tenetur. Amet distinctio eligendi
+        fugit hic impedit magnam mollitia, odit quos? Explicabo impedit maxime minima natus nemo
+        quae quasi, quo ratione saepe voluptas. Labore modi molestiae nobis quia. Culpa deserunt
+        dolor dolorum eum fugiat omnis possimus quaerat quisquam, sequi soluta. Ad aliquid explicabo
+        iste minus perferendis perspiciatis quidem tempora? Accusantium adipisci aliquid autem
+        consequatur dicta doloremque expedita explicabo illum, magni maxime mollitia odit
+        perspiciatis provident, quibusdam quo recusandae reprehenderit, repudiandae sapiente sed
+        tempore! Accusantium aliquam consequuntur debitis est eveniet itaque iure laboriosam magni
+        molestiae nisi praesentium, quae, quasi quisquam veniam voluptas. Corporis facilis incidunt
+        laudantium nam non perferendis quaerat quis sequi veritatis vitae? Aliquid facilis, nobis?
+        At consectetur dolores dolorum eaque iure, laudantium tempora voluptates? Ab fugit
+        reiciendis sunt tempore. Animi quam quidem unde ut. Aliquam dolorum enim maiores quae,
+        suscipit totam. Accusamus adipisci animi dolor doloribus est labore magnam, officia officiis
+        possimus provident quas quos, reprehenderit saepe sequi ullam velit voluptatibus voluptatum.
+        Aliquid aperiam at blanditiis consectetur consequatur deleniti dignissimos doloremque
+        dolorum earum exercitationem facere harum hic inventore iste laborum libero magni maxime,
+        minus molestiae nam necessitatibus nostrum omnis optio possimus quia quibusdam quis quos
+        reiciendis reprehenderit sint tempore, totam vero voluptas. Ad adipisci alias architecto
+        deleniti deserunt dicta, eum exercitationem facere hic illum inventore laborum laudantium,
+        libero, minus nostrum nulla quod ratione recusandae repellat saepe sit tempore temporibus
+        voluptas?
+      </Typography>
+    ),
+    onOpenChange: (open: boolean) => console.log('is open'),
+    open: true,
+    title: 'Title modal with content',
+  },
+}
+
+export const ModalFullExample: Story = {
+  args: {
+    onOpenChange: (open: boolean) => console.log('is open'),
+    open: true,
     title: 'Title modal',
   },
 }
