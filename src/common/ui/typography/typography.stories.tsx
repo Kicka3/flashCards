ad73ca2ab3a81f33a7e404e3f72b37df5d3856f3
NikolosD <alcari2002@gmail.com>
2024-04-18 00:09:16 +0400

    fix: typography styles

--- src/common/ui/typography/typography.stories.tsx
+++ src/common/ui/typography/typography.stories.tsx
@@ -32,6 +32,28 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
+export const AllTypography: Story = {
+  args: {},
+  render: () => {
+    return (
+      <>
+        <Typography variant={'h1'}>Heading 1</Typography>
+        <Typography variant={'h2'}>Heading 2</Typography>
+        <Typography variant={'h3'}>Heading 3</Typography>
+        <Typography variant={'h4'}>Heading 4</Typography>
+        <Typography variant={'sub1'}>Subtitle 1</Typography>
+        <Typography variant={'sub2'}>Subtitle 2</Typography>
+        <Typography variant={'body1'}>Body 1</Typography>
+        <Typography variant={'body2'}>Body 2</Typography>
+        <Typography variant={'overline'}>Overline</Typography>
+        <Typography variant={'caption'}>Caption</Typography>
+        <Typography variant={'link1'}>Link 1</Typography>
+        <Typography variant={'link2'}>Link 2</Typography>
+      </>
+    )
+  },
+}
+
 export const Heading1: Story = {
   args: {
     as: 'h1',
