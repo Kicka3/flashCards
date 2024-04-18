34d092c5af2c8addb68252db0d3f47ddc9198e67
Kirill <kirill_ozornin@mail.ru>
2024-04-18 21:13:59 +0500

    fix: fix folders 2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,15 @@
 import { Router } from '@/app/router/router'
+import { TextField } from '@/common/ui/textField'
 
 export function App() {
   return (
     <>
       <Router />
+      <div>
+        <TextField disabled value={'wqeqweqw'} variant={'search'} />
+        <TextField disabled value={'222222'} variant={'default'} />
+        <TextField disabled value={'3333333'} variant={'password'} />
+      </div>
     </>
   )
 }
