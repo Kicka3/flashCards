32b90431fab40190ddf0773c57ee86adbfc720d5
Vaone <gvin8888@gmail.com>
2024-04-21 23:57:54 +0500

    FLAS-22-Page-Error-404: add storybook pageNotFound

---
+++ src/pages/pageNotFound/pageNotFound.stories.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import Layout from '@/app/layout/layout'
+import { Meta, StoryObj } from '@storybook/react'
+
+import PageNotFound from './pageNotFound'
+
+const meta = {
+  argTypes: {},
+  component: PageNotFound,
+  decorators: [
+    Story => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+  tags: ['autodocs'],
+  title: 'Pages/PageNotFound',
+} satisfies Meta<typeof PageNotFound>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const PageNotFoundDefault: Story = {
+  args: {},
+  render: () => {
+    return (
+      <Layout>
+        <PageNotFound />
+      </Layout>
+    )
+  },
+}
