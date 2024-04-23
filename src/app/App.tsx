70861844db2d8f8369c3fa0a301236e332c5ed32
NikolosD <alcari2002@gmail.com>
2024-04-23 17:50:00 +0400

    FLAS-37 init: add store and base api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,12 @@
+import { Provider } from 'react-redux'
+
 import { Router } from '@/app/router'
+import { store } from '@/services/store'
 
 export function App() {
-  return <Router />
+  return (
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  )
 }
