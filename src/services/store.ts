f838c337672738b4690d317d0c97ad50da4ac101
NikolosD <alcari2002@gmail.com>
2024-04-23 20:12:05 +0400

    FLAS-38 refactor: deleted commentary

--- src/services/store.ts
+++ src/services/store.ts
@@ -13,6 +13,5 @@ export const store = configureStore({
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
