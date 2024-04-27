134295283fea50fa50b66ec5eead02433a05dd82
Kirill <kirill_ozornin@mail.ru>
2024-04-27 12:59:12 +0500

    FLAS-42 hot fix: add msw

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,20 @@ import './styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+
+  const { worker } = await import('./services/mocks/browser')
+
+  return worker.start()
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
--- src/pages/auth/signIn/signIn.stories.tsx
+++ src/pages/auth/signIn/signIn.stories.tsx
@@ -5,6 +5,18 @@ import { SignIn } from '@/pages/auth/signIn/signIn'
 import { store } from '@/services/store'
 import { Meta, StoryObj } from '@storybook/react'
 
+// async function enableMocking() {
+//   if (process.env.NODE_ENV !== 'development') {
+//     return
+//   }
+//
+//   const { worker } = await import('../../../services/mocks/browser')
+//
+//   // `worker.start()` returns a Promise that resolves
+//   // once the Service Worker is up and ready to intercept requests.
+//   return worker.start()
+// }
+
 const meta = {
   argTypes: {},
   component: SignIn,
@@ -32,3 +44,31 @@ export const ExampleSignIn: Story = {
     return <SignIn />
   },
 }
+
+// const meta = {
+//   argTypes: {},
+//   component: SignIn,
+//   decorators: [
+//     Story => (
+//       <BrowserRouter>
+//         <Provider store={store}>
+//           <Story />
+//         </Provider>
+//       </BrowserRouter>
+//     ),
+//   ],
+//   tags: ['autodocs'],
+//   title: 'Auth/SignIn',
+// } satisfies Meta<typeof SignIn>
+//
+// export default meta
+// type Story = StoryObj<typeof meta>
+//
+// export const ExampleSignIn: Story = {
+//   args: {
+//     children: 'TEST COMPONENT',
+//   },
+//   render: () => {
+//     return <SignIn />
+//   },
+// }
