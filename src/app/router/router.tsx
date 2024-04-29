79aa06b8d11e413e8d364d47312597b691dcf6f8
Kirill <kirill_ozornin@mail.ru>
2024-04-29 19:24:17 +0500

    feat: add router. again.

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,79 +1,92 @@
-import {
-  Navigate,
-  Outlet,
-  RouteObject,
-  RouterProvider,
-  createBrowserRouter,
-} from 'react-router-dom'
+// import {
+//   Navigate,
+//   Outlet,
+//   RouteObject,
+//   RouterProvider,
+//   createBrowserRouter,
+// } from 'react-router-dom'
+//
+// import { CheckEmail } from '@/pages/auth/checkEmail'
+// import { CreateNewPassword } from '@/pages/auth/createNewPassword'
+// import { ForgotPassword } from '@/pages/auth/forgotPasword'
+// import { SignIn } from '@/pages/auth/signIn'
+// import { SignUp } from '@/pages/auth/singUp'
+// import { MockPack } from '@/pages/mockPack'
+// import { Cards } from '@/pages/mockPack/Pack'
+// import PageNotFound from '@/pages/pageNotFound/pageNotFound'
+//
+// import Layout from '../layout/layout'
+//
+// const publicRoutes: RouteObject[] = [
+//   {
+//     element: <ForgotPassword />,
+//     path: '/forgotPassword',
+//   },
+//   {
+//     element: <SignIn />,
+//     path: '/signIn',
+//   },
+//   {
+//     element: <SignUp />,
+//     path: '/signUp',
+//   },
+//   {
+//     element: <CheckEmail email={'mail@mail.com'} />,
+//     path: '/checkEmail',
+//   },
+//   {
+//     element: <CreateNewPassword />,
+//     path: '/createNewPassword',
+//   },
+// ]
+//
+// const privateRoutes: RouteObject[] = [
+//   {
+//     children: [
+//       {
+//         element: <MockPack />,
+//         path: '/packs',
+//       },
+//       {
+//         element: <Cards />,
+//         path: '/packs/:id',
+//       },
+//     ],
+//   },
+// ]
+//
+// const router = createBrowserRouter([
+//   {
+//     children: [{ children: privateRoutes, element: <PrivateRoutes /> }, { children: publicRoutes }],
+//     element: <Layout />,
+//     errorElement: (
+//       <Layout>
+//         <PageNotFound />
+//       </Layout>
+//     ),
+//     path: '/',
+//   },
+// ])
+//
+// export const Router = () => {
+//   return <RouterProvider router={router} />
+// }
+//
+// const isAuthenticated = true
+//
+// function PrivateRoutes() {
+//   return isAuthenticated ? <Outlet /> : <Navigate to={'/signIn'} />
+// }
 
-import { CheckEmail } from '@/pages/auth/checkEmail'
-import { CreateNewPassword } from '@/pages/auth/createNewPassword'
-import { ForgotPassword } from '@/pages/auth/forgotPasword'
-import { SignIn } from '@/pages/auth/signIn'
-import { SignUp } from '@/pages/auth/singUp'
-import { MockPack } from '@/pages/mockPack'
-import { Cards } from '@/pages/mockPack/Pack'
-import PageNotFound from '@/pages/pageNotFound/pageNotFound'
-
-import Layout from '../layout/layout'
-
-const publicRoutes: RouteObject[] = [
-  {
-    element: <ForgotPassword />,
-    path: '/forgotPassword',
-  },
-  {
-    element: <SignIn />,
-    path: '/signIn',
-  },
-  {
-    element: <SignUp />,
-    path: '/signUp',
-  },
-  {
-    element: <CheckEmail email={'mail@mail.com'} />,
-    path: '/checkEmail',
-  },
-  {
-    element: <CreateNewPassword />,
-    path: '/createNewPassword',
-  },
-]
-
-const privateRoutes: RouteObject[] = [
-  {
-    children: [
-      {
-        element: <MockPack />,
-        path: '/packs',
-      },
-      {
-        element: <Cards />,
-        path: '/packs/:id',
-      },
-    ],
-  },
-]
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
-    children: [{ children: privateRoutes, element: <PrivateRoutes /> }, { children: publicRoutes }],
-    element: <Layout />,
-    errorElement: (
-      <Layout>
-        <PageNotFound />
-      </Layout>
-    ),
+    element: <div>H3ll0</div>,
     path: '/',
   },
 ])
 
 export const Router = () => {
   return <RouterProvider router={router} />
-}
-
-const isAuthenticated = true
-
-function PrivateRoutes() {
-  return isAuthenticated ? <Outlet /> : <Navigate to={'/signIn'} />
 }
