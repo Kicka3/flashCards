4468edcb1098f78dca0aa80c97054da79f30e548
NikolosD <alcari2002@gmail.com>
2024-05-05 14:16:38 +0400

    FLAS-54 fix: add base64 in func

--- src/pages/auth/editProfile/profileInfo/utils/fileChange.ts
+++ src/pages/auth/editProfile/profileInfo/utils/fileChange.ts
@@ -2,7 +2,7 @@ import { ChangeEvent } from 'react'
 
 export const handleFileChange = async (
   event: ChangeEvent<HTMLInputElement>,
-  updateAvatar: (file: string) => Promise<string>,
+  updateAvatar: (base64String: string) => Promise<string>,
   setAvatarImage: (url: string) => void,
   onAvatarChange: (url: null | string) => void
 ) => {
@@ -20,18 +20,21 @@ export const handleFileChange = async (
     const reader = new FileReader()
 
     reader.onloadend = async () => {
-      const base64String = reader.result as string
+      if (reader.result) {
+        const base64String = reader.result as string
 
-      try {
-        const uploadedAvatarUrl = await updateAvatar(base64String)
+        try {
+          const uploadedAvatarUrl = await updateAvatar(base64String)
 
-        setAvatarImage(uploadedAvatarUrl)
-        onAvatarChange(uploadedAvatarUrl)
-      } catch (error) {
-        console.error('Error uploading avatar:', error)
-        onAvatarChange(null)
+          setAvatarImage(uploadedAvatarUrl)
+          onAvatarChange(uploadedAvatarUrl)
+        } catch (error) {
+          console.error('Error uploading avatar:', error)
+          onAvatarChange(null)
+        }
       }
     }
+
     reader.readAsDataURL(file)
   }
 }
