b4a3d89d4b14dd41b24f59db91174cfa3161f869
Kirill <kirill_ozornin@mail.ru>
2024-05-10 11:25:28 +0500

    FLAS-39: feat: add convert data from

--- src/services/decks/decks.service.ts
+++ src/services/decks/decks.service.ts
@@ -6,13 +6,41 @@ import {
   GetDecksResponse,
 } from '@/services/decks/decks.types'
 
+/** конвертирую в FormData */
+const deckFormDataHandler = (data: DeckBodyRequest) => {
+  const formData = new FormData()
+
+  if (data.cover instanceof File) {
+    formData.append('cover', data.cover)
+  } else if (data.cover === null) {
+    formData.append('cover', '')
+  }
+  formData.append('name', data.name)
+  formData.append('isPrivate', String(data.isPrivate))
+
+  return formData
+}
+
 export const decksApiService = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
       createDeck: builder.mutation<void, DeckBodyRequest>({
         invalidatesTags: ['Decks'],
+        // async onQueryStarted(_, { dispatch, getState, queryFulfilled }) {
+        //   const res = await queryFulfilled
+        //
+        //   const args = decksService.util.selectCachedArgsForQuery(getState(), 'getDecks')
+        //
+        //   dispatch(
+        //     decksService.util.updateQueryData('getDecks', args[0], draft => {
+        //       draft.items.pop()
+        //       draft.items.unshift(res.data)
+        //     })
+        //   )
+        // },
         query: args => ({
-          body: args,
+          //Конверитрую датаформ и отправляю
+          body: deckFormDataHandler(args),
           method: 'POST',
           url: 'v1/decks',
         }),
