fda1ba9638586ff041d00fb706aa5be03e9b76ce
Vaone <gvin8888@gmail.com>
2024-05-12 18:58:40 +0500

    FLAS-48-validationFix: - some validation

--- src/pages/auth/createNewPassword/utils/createNewPasswordSchema.ts
+++ src/pages/auth/createNewPassword/utils/createNewPasswordSchema.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod'
 
 export const createNewPasswordSchema = z.object({
-  password: z.string().min(3),
+  password: z
+    .string()
+    .min(3, 'Password must be at least 3 characters long')
+    .max(100, 'Password must be less than 100 characters'),
 })
 
 export type NewPasswordFormValues = z.infer<typeof createNewPasswordSchema>
