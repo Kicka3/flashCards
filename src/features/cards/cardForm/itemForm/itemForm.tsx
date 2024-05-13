7b477a0fcd3bf8a5e7d79b1a4e6049d977564723
Vaone <gvin8888@gmail.com>
2024-05-14 00:41:31 +0500

    FLAS-40 fix: - form createCard/updateCard validation

--- src/features/cards/cardForm/itemForm/itemForm.tsx
+++ src/features/cards/cardForm/itemForm/itemForm.tsx
@@ -3,23 +3,25 @@ import { Control } from 'react-hook-form'
 
 import { Image } from '@/assets/icons/components'
 import { ImageFile } from '@/common/types/types'
+import { TextFieldProps } from '@/common/ui'
 import { Button } from '@/common/ui/button'
 import { ControlledTextField } from '@/common/ui/controlled/controlled-textField'
 import { openFiles } from '@/common/utils'
 
 import s from './ItemForm.module.scss'
 
-import { AddCardFormValues } from '../CardFormSchema'
+import { CardFormValues } from '../CardFormSchema'
 
 type Props = {
-  control: Control<AddCardFormValues, any>
+  control: Control<CardFormValues, any>
+  errorMessage?: string
   image: ImageFile
   label: string
   name: 'answer' | 'question'
   setImage: (image: ImageFile) => void
 }
 
-export const ItemForm = ({ control, image, label, name, setImage }: Props) => {
+export const ItemForm = ({ control, errorMessage, image, label, name, setImage }: Props) => {
   const ref = useRef<HTMLInputElement>(null)
 
   const onChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
@@ -30,7 +32,13 @@ export const ItemForm = ({ control, image, label, name, setImage }: Props) => {
 
   return (
     <div className={s.itemWrapper}>
-      <ControlledTextField control={control} label={label} name={name} variant={'default'} />
+      <ControlledTextField
+        control={control}
+        errorMessage={errorMessage}
+        label={label}
+        name={name}
+        variant={'default'}
+      />
 
       <div className={s.imageWrapper}>
         {image === null ? (
