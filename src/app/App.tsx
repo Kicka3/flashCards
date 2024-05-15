7902f50800b9f540e7b83091e4223218e3143327
Kirill <kirill_ozornin@mail.ru>
2024-05-15 20:16:58 +0500

    Hot-fix-dev feat: delete SpeedInsights

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,11 @@ import { Provider } from 'react-redux'
 
 import { Router } from '@/app/router'
 import { store } from '@/services/store'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export function App() {
   return (
     <Provider store={store}>
       <Router />
-      <SpeedInsights />
     </Provider>
   )
 }
