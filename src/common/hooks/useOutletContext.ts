af1b962f6d72754ad9601a423bf863b6cbc61a40
NikolosD <alcari2002@gmail.com>
2024-05-23 17:49:00 +0400

    bug-fix-nik fix: router issues

--- src/common/hooks/useOutletContext.ts
+++ src/common/hooks/useOutletContext.ts
@@ -1,11 +1,7 @@
 import { useOutletContext } from 'react-router-dom'
 
-import { UserData } from '@/services/auth'
-
 export type AppOutletContext = {
   isAuth: boolean
-  isLoading: boolean
-  user: UserData | null
 }
 
 export const useAppOutletContext = () => useOutletContext<AppOutletContext>()
